5fdddabb92a9faf445e51b9b7f3d81ed8970af5c
Dobrvnak <Dobrvnak@gmail.com>
2026-07-20 20:39:13 +0300

    Remove useless 'elif', brackets and outputting type(input)

--- lessons/age_check.py
+++ lessons/age_check.py
@@ -1,12 +1,9 @@
 year_of_birth = int(input("Укажите ваш год рождения "))
 current_year = 2026
-age = (current_year - year_of_birth)
+age = current_year - year_of_birth
 if age >= 18:
     print("Ты уже взрослый и наверно самостоятельный человек")
-elif age == 17:
-    print("Ты ещё школьник")
 elif age >= 14:
     print("Ты ещё школьник")
 else:
     print("У тебя ещё молоко на губах не обсохло")
-print(type(input))
