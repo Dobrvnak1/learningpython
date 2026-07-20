79593696940305329a8cb6287b88b0ba89b684d8
Dobrvnak <Dobrvnak@gmail.com>
2026-07-20 20:26:55 +0300

    Fix 'elif', change variable from age to year_of_birth

--- lessons/age_check.py
+++ lessons/age_check.py
@@ -1,10 +1,12 @@
-age = int(input("Укажите ваш год рождения "))
+year_of_birth = int(input("Укажите ваш год рождения "))
 current_year = 2026
-age = (current_year - age)
-print(age)
+age = (current_year - year_of_birth)
 if age >= 18:
     print("Ты уже взрослый и наверно самостоятельный человек")
-elif age <= 18:
+elif age == 17:
+    print("Ты ещё школьник")
+elif age >= 14:
     print("Ты ещё школьник")
 else:
     print("У тебя ещё молоко на губах не обсохло")
+print(type(input))
