6a9398e7e7a558b9cb58e2edbb8184ab3092a1dd
Dobrvnak <Dobrvnak@gmail.com>
2026-07-21 16:55:31 +0300

    Add limited number of attempts and not counting empty lines

--- lessons/guess_number_modified.py
+++ lessons/guess_number_modified.py
@@ -1,13 +1,13 @@
 hidden_number = 7
 attempts = 0
 while True:
-    input_number = input("Угадай число от 1 до 10: ")
+    input_number = (input("Угадай число от 1 до 10: "))
     if input_number == "":
         print("Пустая строка не считается '_' ")
         continue
     input_number = int(input_number)
-    attempts += 1
-    if input_number == hidden_number:
+    attempts = attempts + 1
+    if  input_number == hidden_number:
         print(f"Поздравляю ты угадал число {hidden_number} за {attempts} раз")
         break
     if attempts == 5:
